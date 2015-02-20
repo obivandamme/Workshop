3d8654a27fb015e486d6e246897d5b4e9d7b50fc
obivandamme <obivandamme@gmail.com>
2015-02-20 09:44:46 +0100

    Updated References and ignore file

--- Source/Workshop/Workshop/OSEModuleWorkshop.cs
+++ Source/Workshop/Workshop/OSEModuleWorkshop.cs
@@ -4,8 +4,9 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    using UnityEngine;
     using KAS;
+
+    using UnityEngine;
 
     public class OseModuleWorkshop : PartModule
     {

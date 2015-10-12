2c13375c6f018625828c6cab4d333d4fe6adb649
obivandamme <obivandamme@gmail.com>
2015-10-12 16:35:28 +0200

    implemented GetProgress

--- Source/Workshop/Workshop/Recipes/Blueprint.cs
+++ Source/Workshop/Workshop/Recipes/Blueprint.cs
@@ -2,8 +2,17 @@
 
 namespace Workshop.Recipes
 {
+    using System.Linq;
+
     public class Blueprint : List<WorkshopResource>, IConfigNode
     {
+        public double GetProgress()
+        {
+            var totalAmount = this.Sum(r => r.Units);
+            var totalProcessed = this.Sum(r => r.Processed);
+            return totalProcessed / totalAmount;
+        }
+
         public void Load(ConfigNode node)
         {
             foreach (var configNode in node.GetNodes("WorkshopResource"))

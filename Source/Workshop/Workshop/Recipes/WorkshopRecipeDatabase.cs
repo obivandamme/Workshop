64778667344a49b6a6208a555f38a0d783c27d58
obivandamme <obivandamme@gmail.com>
2015-10-12 11:54:26 +0200

    Added loader

---
+++ Source/Workshop/Workshop/Recipes/WorkshopRecipeDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Workshop.Recipes
+{
+    using UnityEngine;
+
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    public class WorkshopRecipeDatabase : MonoBehaviour
+    {
+        public static Dictionary<string, Recipe> PartRecipes;
+
+        public static Dictionary<string, Recipe> ResourceRecipes;
+
+        public static List<WorkshopResource> ProcessPart(Part part)
+        {
+            var resources = new Dictionary<string, WorkshopResource>();
+            if (PartRecipes.ContainsKey(part.name))
+            {
+                var recipe = PartRecipes[part.name];
+                foreach (var workshopResource in recipe.Prepare(part.mass))
+                {
+                    if (resources.ContainsKey(workshopResource.Name))
+                    {
+                        resources[workshopResource.Name].Merge(workshopResource);
+                    }
+                    else
+                    {
+                        resources[workshopResource.Name] = workshopResource;
+                    }
+                }
+            }
+
+            foreach (PartResource partResource in part.Resources)
+            {
+                if (ResourceRecipes.ContainsKey(partResource.resourceName))
+                {
+                    var recipe = ResourceRecipes[partResource.resourceName];
+                    foreach (var workshopResource in recipe.Prepare(part.mass))
+                    {
+                        if (resources.ContainsKey(workshopResource.Name))
+                        {
+                            resources[workshopResource.Name].Merge(workshopResource);
+                        }
+                        else
+                        {
+                            resources[workshopResource.Name] = workshopResource;
+                        }
+                    }
+                }
+            }
+            
+            return resources.Values.ToList();
+        }
+
+        void Awake()
+        {
+            PartRecipes = new Dictionary<string, Recipe>();
+            ResourceRecipes = new Dictionary<string, Recipe>();
+
+            var loaders = LoadingScreen.Instance.loaders;
+            if (loaders != null)
+            {
+                for (var i = 0; i < loaders.Count; i++)
+                {
+                    var loadingSystem = loaders[i];
+                    if (loadingSystem is WorkshopRecipeLoader)
+                    {
+                        (loadingSystem as WorkshopRecipeLoader).Done = false;
+                        break;
+                    }
+                    if (loadingSystem is PartLoader)
+                    {
+                        var go = new GameObject();
+                        var recipeLoader = go.AddComponent<WorkshopRecipeLoader>();
+                        loaders.Insert(i, recipeLoader);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

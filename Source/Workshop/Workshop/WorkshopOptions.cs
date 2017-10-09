097aa1314ae71bf79b28a04d7ca2bb17dfcda80e
Aelfhe1m <aelfhe1m@hotmail.com>
2017-10-09 16:23:55 +0100

    Code styling

--- Source/Workshop/Workshop/WorkshopOptions.cs
+++ Source/Workshop/Workshop/WorkshopOptions.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using KSP.IO;
-
-namespace Workshop
+﻿namespace Workshop
 {
     public class WorkshopOptions : GameParameters.CustomParameterNode
     {
@@ -36,50 +29,20 @@ namespace Workshop
         }
 
         #region CustomParameterNode
-        public override string Section
-        {
-            get
-            {
-                return "Workshop";
-            }
-        }
+        public override string Section =>"Workshop";
 
-        public override string Title
-        {
-            get
-            {
-                return "Efficiency";
-            }
-        }
+        public override string Title => "Efficiency";
 
-        public override int SectionOrder
-        {
-            get
-            {
-                return 0;
-            }
-        }
+        public override int SectionOrder => 0;
 
         public override void SetDifficultyPreset(GameParameters.Preset preset)
         {
             base.SetDifficultyPreset(preset);
         }
 
-        public override GameParameters.GameMode GameMode
-        {
-            get
-            {
-                return GameParameters.GameMode.ANY;
-            }
-        }
+        public override GameParameters.GameMode GameMode => GameParameters.GameMode.ANY;
 
-        public override bool HasPresets
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public override bool HasPresets => false;
 
         public override bool Enabled(System.Reflection.MemberInfo member, GameParameters parameters)
         {
